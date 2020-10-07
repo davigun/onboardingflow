301388703976e0f618d47a4c618afdea99c36c50
David <david_gunawan_@hotmail.com>
2020-10-07 12:31:16 +0700

    Initial Commit

---
+++ onboard/onboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  onboardApp.swift
+//  onboard
+//
+//  Created by David Gunawan on 07/10/20.
+//
+
+import SwiftUI
+
+@main
+struct onboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
