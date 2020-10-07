bd7c1af91fd7e66d3364f8e86d2cee6a56a237e2
David <david_gunawan_@hotmail.com>
2020-10-07 15:35:49 +0700

    first commit

---
+++ onboard/LaunchView.swift
@@ -0,0 +1,20 @@
+//
+//  LaunchView.swift
+//  onboard
+//
+//  Created by David Gunawan on 07/10/20.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LaunchView_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchView()
+    }
+}
